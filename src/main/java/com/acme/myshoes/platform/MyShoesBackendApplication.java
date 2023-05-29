b161bd803d7cb6606ba900b0121e5eb3e0e5c1b0
William Riega <williammar954@hotmail.com>
2023-05-29 15:14:14 -0500

    Initial Commit

---
+++ src/main/java/com/acme/myshoes/platform/MyShoesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.myshoes.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShoesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShoesBackendApplication.class, args);
+	}
+
+}

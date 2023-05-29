72c4b2655870256de62e4c731a602bcca337fe88
William Riega <williammar954@hotmail.com>
2023-05-29 16:44:18 -0500

    Feat: Shoe class

---
+++ src/main/java/com/acme/myshoes/platform/shoes/domain/model/Shoe.java
@@ -0,0 +1,33 @@
+package com.acme.myshoes.platform.shoes.domain.model;
+
+import com.acme.myshoes.platform.shared.domain.model.AuditModel;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity //para la creación de su mapeo en base de datos
+@Table(name = "shoe") // especificar el nombre para seguir la convencion correcta
+public class Shoe extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max=20)
+    @Column(unique = true)
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Size(max=20)
+    private String ownerName;
+    private float size;
+}

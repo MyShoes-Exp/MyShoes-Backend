cb235d3d431dc76919b8f04b160d58635641a1d2
FabriHM <u202118067@upc.edu.pe>
2023-06-03 21:58:50 -0500

    chore: update ShoppingCartService

--- src/main/java/com/acme/myshoes/platform/shopping/domain/service/ShoppingCartService.java
+++ src/main/java/com/acme/myshoes/platform/shopping/domain/service/ShoppingCartService.java
@@ -1,5 +1,6 @@
 package com.acme.myshoes.platform.shopping.domain.service;
 
+import com.acme.myshoes.platform.shoes.domain.model.Shoe;
 import com.acme.myshoes.platform.shopping.domain.model.ShoppingCart;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,5 +13,8 @@ public interface ShoppingCartService {
     Page<ShoppingCart> getAll(Pageable pageable);
     ShoppingCart create(ShoppingCart shoppingCart);
     ResponseEntity<?> delete(Long shoppingCartId);
+    void AddShoesToCart(Long shoppingCartId, Shoe shoe);
+    ShoppingCart getById(Long shoppingcartId);
 
+    ShoppingCart update(Long id, ShoppingCart shoppingCart);
 }

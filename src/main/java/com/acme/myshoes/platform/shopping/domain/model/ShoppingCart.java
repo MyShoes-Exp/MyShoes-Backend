82d38834380add02f419c4fb9ae3c225698ffd57
FabriHM <u202118067@upc.edu.pe>
2023-06-03 21:57:47 -0500

    chore: update ShoppingCartModel

--- src/main/java/com/acme/myshoes/platform/shopping/domain/model/ShoppingCart.java
+++ src/main/java/com/acme/myshoes/platform/shopping/domain/model/ShoppingCart.java
@@ -8,13 +8,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
 @With
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "shopping_cart")
@@ -23,14 +24,41 @@ public class ShoppingCart extends AuditModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "shopping_cart")
-    private Set<Shoe> shoes = new HashSet<>();
-    //Se aplica para la rama Shoe tmb
+    //private Long total;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "shoppingCart")
+    private List<Shoe> shoes = new ArrayList<>();
 
     @OneToOne
     @JoinColumn(name="shopping_cart_id", nullable = false)
     @JsonIgnore
-    private Collection collection;
-    //cambiar private
+    private Collection collection;//reemplazar por User
 
+    public ShoppingCart(){
+        shoes = new ArrayList<>();
+    }
+    public void addShoe(Shoe shoe){
+        shoes.add(shoe);
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Shoe> getShoes() {
+        return shoes;
+    }
+
+    public void setShoes(List<Shoe> shoes) {
+        this.shoes = shoes;
+    }
+
+/*    public void getTotal(){
+        for(Shoe shoe : shoes){
+            total += shoe.getPrice();
+        }
+    }*/
 }

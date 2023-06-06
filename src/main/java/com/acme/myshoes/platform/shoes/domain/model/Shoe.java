e611e397a2d51a212b26c8737f17c204cc868a34
FabriHM <102436134+FabriHM@users.noreply.github.com>
2023-06-06 08:12:43 -0500

    fix: delete relationship with ShoppingCart

--- src/main/java/com/acme/myshoes/platform/shoes/domain/model/Shoe.java
+++ src/main/java/com/acme/myshoes/platform/shoes/domain/model/Shoe.java
@@ -33,11 +33,5 @@ public class Shoe extends AuditModel {
     @JoinColumn(name="collection_id", nullable = false)
     @JsonIgnore
     private Collection collection;
-    
-    @ManyToMany
-    @JoinTable(name = "shopping_cart_shoe",
-            joinColumns = @JoinColumn(name = "shoe_id"),
-            inverseJoinColumns = @JoinColumn(name = "shopping_cart_id"))
-    @JsonIgnore
-    private Set<ShoppingCart> shoppingCarts = new HashSet<>();
+
 }

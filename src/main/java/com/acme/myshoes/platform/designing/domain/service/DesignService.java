03546d4939ce0510a546884dbcb2e19487363213
FabriHM <102436134+FabriHM@users.noreply.github.com>
2023-06-06 08:22:37 -0500

    feat: added interface DesignService

---
+++ src/main/java/com/acme/myshoes/platform/designing/domain/service/DesignService.java
@@ -0,0 +1,19 @@
+package com.acme.myshoes.platform.designing.domain.service;
+
+import com.acme.myshoes.platform.shoes.domain.model.Shoe;
+import com.acme.myshoes.platform.designing.domain.model.Design;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface DesignService {
+    List<Design> getAll();
+    Page<Design> getAll(Pageable pageable);
+    Design create(Design design);
+    ResponseEntity<?> delete(Long shoppingCartId);
+    Design getById(Long design_id);
+
+    Design update(Long id, Design design);
+}

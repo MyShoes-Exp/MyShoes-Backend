0962b00b448846ca7ef9c22f3d5b6cc1c21e2671
FabriHM <102436134+FabriHM@users.noreply.github.com>
2023-06-06 08:17:40 -0500

    feat: added Design Model

---
+++ src/main/java/com/acme/myshoes/platform/designing/domain/model/Design.java
@@ -0,0 +1,22 @@
+package com.acme.myshoes.platform.designing.domain.model;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "design")
+public class Design {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long shoe_id;
+    private String color;
+    private String material;
+}

c85623d8240199213feca5742c90f65c3989ebce
Anderson Velasquez <andersonvelezca@gmail.com>
2023-06-06 03:10:06 -0500

    feat: added service from Order

--- src/main/java/com/acme/myshoes/platform/orders/domain/service/OrderService.java
+++ src/main/java/com/acme/myshoes/platform/orders/domain/service/OrderService.java
@@ -1,2 +1,26 @@
-package com.acme.myshoes.platform.orders.domain.service;public interface OrderService {
+package com.acme.myshoes.platform.orders.domain.service;
+
+import com.acme.myshoes.platform.orders.domain.model.Order;
+import com.acme.myshoes.platform.shoes.domain.model.Shoe;
+import com.acme.myshoes.platform.shoes.domain.model.User;
+import com.acme.myshoes.platform.shopping.domain.model.ShoppingCart;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface OrderService {
+
+    List<Order> getAll();
+    Page<Order> getAll(Pageable pageable);
+    Order create(Order order);
+
+    Order getById(Long orderId);
+
+    Order update(Long id, Order order);
+    ResponseEntity<?> delete(Long orderId);
+
+    Order AddUserToOrder(Long orderId, User user);
+
 }

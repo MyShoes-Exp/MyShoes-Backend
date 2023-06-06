a87bab404453758c22c42f12c690820d23eadf1b
FabriHM <u202118067@upc.edu.pe>
2023-06-05 21:27:09 -0500

    feat: added imports

--- src/main/java/com/acme/myshoes/platform/shoes/domain/model/Shoe.java
+++ src/main/java/com/acme/myshoes/platform/shoes/domain/model/Shoe.java
@@ -1,12 +1,16 @@
 package com.acme.myshoes.platform.shoes.domain.model;
 
 import com.acme.myshoes.platform.shared.domain.model.AuditModel;
+import com.acme.myshoes.platform.shopping.domain.model.ShoppingCart;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter

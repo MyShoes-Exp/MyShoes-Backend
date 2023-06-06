a292591bba3737be4b7bc0b26e15f1239cc599e8
FabriHM <102436134+FabriHM@users.noreply.github.com>
2023-06-06 08:24:24 -0500

    feat: added DesignResource

---
+++ src/main/java/com/acme/myshoes/platform/designing/resource/DesignResource.java
@@ -0,0 +1,13 @@
+package com.acme.myshoes.platform.designing.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DesignResource {
+    private Long id;
+}
---
+++ src/main/java/com/acme/myshoes/platform/designing/resource/UpdateDesignResource.java
@@ -0,0 +1,10 @@
+package com.acme.myshoes.platform.designing.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateDesignResource {
+    private Long id;
+}

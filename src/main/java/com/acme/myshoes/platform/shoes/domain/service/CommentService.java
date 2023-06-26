fa19335923d5cd3312e7065f5dc17514f5a1c2a4
FrancoSurco <u202015132@upc.edu.pe>
2023-06-25 22:23:07 -0500

    fix: Updated commentService

--- src/main/java/com/acme/myshoes/platform/shoes/domain/service/CommentService.java
+++ src/main/java/com/acme/myshoes/platform/shoes/domain/service/CommentService.java
@@ -6,9 +6,11 @@ import com.acme.myshoes.platform.shoes.domain.model.Shoe;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CommentService {
     List<Comment> getAll();
     Page<Comment> getAll(Pageable pageable);

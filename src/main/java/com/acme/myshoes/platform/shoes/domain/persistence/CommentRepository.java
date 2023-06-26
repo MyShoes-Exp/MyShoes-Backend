898addeb2d2f215f5f08c2ccfb71bbf52ff681e3
FrancoSurco <u202015132@upc.edu.pe>
2023-06-25 22:57:11 -0500

    fix: Update Comment SeviceImpl

--- src/main/java/com/acme/myshoes/platform/shoes/domain/persistence/CommentRepository.java
+++ src/main/java/com/acme/myshoes/platform/shoes/domain/persistence/CommentRepository.java
@@ -2,9 +2,11 @@ package com.acme.myshoes.platform.shoes.domain.persistence;
 
 import com.acme.myshoes.platform.shoes.domain.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     Optional<Comment> findByName(String name);
 }

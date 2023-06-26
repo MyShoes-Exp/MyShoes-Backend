727fbaddc7dcfeed928cda97b2568d76c4e91c5b
FrancoSurco <u202015132@upc.edu.pe>
2023-06-25 22:31:41 -0500

    feat: added comentController

--- src/main/java/com/acme/myshoes/platform/shoes/api/rest/CommentController.java
+++ src/main/java/com/acme/myshoes/platform/shoes/api/rest/CommentController.java
@@ -2,13 +2,11 @@ package com.acme.myshoes.platform.shoes.api.rest;
 
 import com.acme.myshoes.platform.shoes.domain.service.CommentService;
 import com.acme.myshoes.platform.shoes.mapping.CommentMapper;
-import com.acme.myshoes.platform.shoes.resource.CommentResource;
-import com.acme.myshoes.platform.shoes.resource.ShoeResource;
+import com.acme.myshoes.platform.shoes.resource.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/comments", produces = "application/json")
@@ -25,4 +23,24 @@ public class CommentController {
     public Page<CommentResource> getAllComments(Pageable pageable) {
         return commentMapper.modelListPage(commentService.getAll(), pageable);
     }
+    @GetMapping("{commentId}")
+    public CommentResource getCommentById(@PathVariable Long commentId) {
+        return commentMapper.toResource(commentService.getById(commentId));
+    }
+
+    @PostMapping
+    public CommentResource createComment(@RequestBody CreateCommentResource resource) {
+        return commentMapper.toResource(commentService.create(commentMapper.toModel(resource)));
+    }
+
+    @PutMapping("{commentId}")
+    public CommentResource updateComment(@PathVariable Long commentId, @RequestBody UpdateCommentResource resource) {
+        return commentMapper.toResource(commentService.update(commentId, commentMapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable Long commentId) {
+        return commentService.delete(commentId);
+    }
+
 }

397f47ba75c0c9eeb86b708f51cb169a857c90ac
FabriHM <u202118067@upc.edu.pe>
2023-06-25 23:15:35 -0500

    add: added ApplicationTests

---
+++ src/test/java/com/acme/myshoes/platform/MyShoesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.myshoes.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShoesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
